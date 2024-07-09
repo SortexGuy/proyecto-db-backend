468fd8c74717cf4d3928fd9e531679004448d3a5
Rubén A <sortexjuegos@gmail.com>
2024-07-09 17:54:43 -0400

    feat(models): Actualizar y agregar modelos faltantes

--- src/models/repositories/teacher.ts
+++ src/models/repositories/teacher.ts
@@ -1,5 +1,7 @@
 import { Teacher } from "@/models/teacher";
+import { ExtCharge } from "@/models/charge";
 
 export interface TeacherRepository {
 	getTeacherByUserId(id: string): Promise<Teacher | null>;
+	getTeacherAcademicChargesByTeacherId(id: string): Promise<ExtCharge[] | null>;
 }
