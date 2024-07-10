858b6db1e3fed9cb8be908365014777d49d99f0f
Rubén A <sortexjuegos@gmail.com>
2024-07-10 17:02:25 -0400

    feat(repos): Representante con capacidad de ver sus representados

--- src/models/repositories/representative.ts
+++ src/models/repositories/representative.ts
@@ -1,5 +1,7 @@
 import { Representative } from "@/models/representative";
+import { Student } from "@/models/student";
 
 export interface RepresentativeRepository {
 	getRepresentativeByUserId(id: string): Promise<Representative | null>;
+	getStudentsByRepresentativeId(id: string): Promise<Student[]>;
 }
