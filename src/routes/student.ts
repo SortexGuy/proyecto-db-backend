f5d982e09d854c3119a084cb4cb998096a656a68
Rubén A <sortexjuegos@gmail.com>
2024-07-11 17:51:23 -0400

    feat: Obtener calificaciones de un estudiante por id

--- src/routes/student.ts
+++ src/routes/student.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
-import { studentRepository } from "../dependencies";
+import { qualificationRepository, studentRepository } from "@/dependencies";
 
 const student = new Hono();
 
@@ -12,6 +12,21 @@ student.get("/:id", async (c) => {
 	}
 
 	return c.json(foundStudent);
+});
+
+student.get("/:id/qualifications", async (c) => {
+	const id = c.req.param("id");
+	const foundStudent = await studentRepository.getStudentById(id);
+	if (!foundStudent) {
+		throw new HTTPException(404, { message: "Student not found" });
+	}
+
+	const qualifications = await qualificationRepository.getQualificationsByStudentId(id);
+	if (!qualifications) {
+		throw new HTTPException(404, { message: "Student not found" });
+	}
+
+	return c.json(qualifications);
 });
 
 export default student;
